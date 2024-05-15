50e1d3045599a1631c56a3ce6c7ec18f4f751bb9
Deven <devenj185@gmail.com>
2024-05-15 20:02:13 +0530

    created serializers for user authentication

--- backend/settings.py
+++ backend/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountApp',
     'corsheaders',
+    
     'rest_framework',
+    'rest_framework.authtoken',
+    
+    'accountApp',
+    'core',
+    
+    
 ]
 
 
@@ -124,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ core/views.py
@@ -0,0 +1,27 @@
+from rest_framework import generics
+from .serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from core import serializers
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    
+class LoginView(ObtainAuthToken):
+    
+    serializer_class = AuthTokenSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializers = self.serializer_class(data=request.data, context={'request':request})
+        serializers.is_valid(raise_exception=True)
+        user = serializers.validated_data['user']
+        token,Created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'email': user.email,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+        })
