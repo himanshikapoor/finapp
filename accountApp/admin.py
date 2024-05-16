749719e11154f5f1a7b9a8a5837b636da2d8b30a
Deven <devenj185@gmail.com>
2024-05-16 18:27:03 +0530

    small change in db

--- accountApp/admin.py
+++ accountApp/admin.py
@@ -3,7 +3,7 @@ from .models import User
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'username', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'is_active', 'date_joined')
     ordering = ('-date_joined',) 
     filter_horizontal = ()
     list_filter = ()
